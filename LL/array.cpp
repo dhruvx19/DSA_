25d9ce274857b4e1c83ded6404090d2d2c1eda5a
Dhruv Balchandani <119007749+dhruvx19@users.noreply.github.com>
2024-01-03 18:37:54 +0530

    array

--- LL/array.cpp
+++ LL/array.cpp
@@ -20,24 +20,67 @@ struct Node{
 
 };
 
+
+
 Node* convertArr2LL(int arr[], int size) {
      Node* head = new Node(arr[0]);
      Node* mover = head;
      for(int i=1;i<size; i++){
         Node* temp = new Node(arr[i]);
         mover->next = temp;
-        mover->next = mover;
+        mover = mover->next;
      }
      return head;
+}
+
+
+
+int lengthOfLL(Node* head){
+    int count = 0;
+    Node* temp = head;
+    while(temp){
+        temp = temp->next;
+        count++;
+    }
+    return count;
+}
+
+
+int checkNumber(Node* head, int val){
+    Node* temp = head;
+    while(temp){
+        if(temp->data == val) {
+            return 1;
+        temp = temp->next;
+        }
+    }
+    return 0;
 }
 
 
 int main() {
     int numbers[] = {2, 3, 5, 7};
         int size = sizeof(numbers) / sizeof(numbers[0]);
+    
 
     Node* head = convertArr2LL(numbers, size);
-    cout << head->data;
+    cout<< head->data <<endl;
 
-    return 0;
+
+
+    // Traversal LL
+    Node* temp = head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+
+    //Lengh Of LL
+    //cout<<lengthOfLL(head)<<endl;
+
+
+    //check number
+    cout << checkNumber(head, 5);
+
+   
 }
